f9fd0bf98d946f9c6cdd78a21de3ecbe084db78e
Pedro Nunes <99264609+nunes262@users.noreply.github.com>
2024-11-18 20:27:17 -0300

    Update UserRepository.java

--- src/main/java/com/example/Qbank/repositories/UserRepository.java
+++ src/main/java/com/example/Qbank/repositories/UserRepository.java
@@ -1,15 +1,49 @@
 package com.example.Qbank.repositories;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.Qbank.model.Account;
 import com.example.Qbank.model.User;
-import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+public class UserRepository {
+    private Map<String, User> users = new HashMap<>();
+    private Map<Long, Account> accounts = new HashMap<>();
+    private Long idCounter = 1L;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    
-    Optional<User> findByEmail(String email);
-    
-    boolean existsByEmail(String email);
+    public void save(User user) {
+        user = new User(idCounter++, user.getName(), user.getCpf(), user.getEmail(), user.getPassword());
+        users.put(user.getEmail(), user);
+        accounts.put(user.getId(), new Account(user.getId())); // Cria uma conta para o usuário
+    }
+
+    public User findByEmail(String email) {
+        return users.get(email);
+    }
+
+    public Account findAccountByUserId(Long userId) {
+        return accounts.get(userId);
+    }
+
+    public boolean existsByEmail(String email) {
+        return users.containsKey(email);
+    }
 }
+
+
+
+// package com.example.Qbank.repositories;
+
+// import com.example.Qbank.model.User;
+// import io.micronaut.data.annotation.Repository;
+// import io.micronaut.data.jpa.repository.JpaRepository;
+
+// import java.util.Optional;
+
+// @Repository
+// public interface UserRepository extends JpaRepository<User, Long> {
+    
+//     Optional<User> findByEmail(String email);
+    
+//     boolean existsByEmail(String email);
+// }
